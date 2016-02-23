0c0619e66174a7d78219edeb20fee957d21071db
Hisham <hisham.dbd@gmail.com>
2016-02-23 10:26:25 +0530

    + Added OKHttp sample of getting and posting data on server.

--- app/src/main/java/com/hisham/navview/MainActivity.java
+++ app/src/main/java/com/hisham/navview/MainActivity.java
@@ -2,21 +2,37 @@ package com.hisham.navview;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private DrawerLayout drawer;
+    private OkHttpClient client = new OkHttpClient();
+    private TextView txtResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +58,87 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+
+        FloatingActionButton btnGetUrl = (FloatingActionButton) findViewById(R.id.btnGetUrl);
+        FloatingActionButton btnPostToServer = (FloatingActionButton) findViewById(R.id.btnPostToServer);
+        btnGetUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Request request = new Request.Builder()
+                        .url("https://raw.github.com/square/okhttp/master/README.md")
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        final String data = response.body().string();
+                        Log.e(TAG, data);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtResponse.setText(data);
+                            }
+                        });
+                    }
+                });
+
+
+            }
+        });
+
+        btnPostToServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String json = bowlingJson("Hisham", "Fola");
+                RequestBody body = RequestBody.create(JSON, json);
+                final Request request = new Request.Builder()
+                        .url("http://www.roundsapp.com/post")
+                        .post(body)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        final String data = response.body().string();
+                        Log.e(TAG, data);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtResponse.setText(data);
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
     }
+
+
+    String bowlingJson(String player1, String player2) {
+        return "{'winCondition':'HIGH_SCORE',"
+                + "'name':'Bowling',"
+                + "'round':4,"
+                + "'lastSaved':1367702411696,"
+                + "'dateStarted':1367702378785,"
+                + "'players':["
+                + "{'name':'" + player1 + "','history':[10,8,6,7,8],'color':-13388315,'total':39},"
+                + "{'name':'" + player2 + "','history':[6,10,5,10,7],'color':-48060,'total':38}"
+                + "]}";
+    }
+
 
     @Override
     public void onBackPressed() {
